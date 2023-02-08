fd300806f5d8e08a8319f860220874ce89e6f6ad
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-08 12:10:01 -0300

    update css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import './App.css'
+
 import Compressor from './components/Compressor';
 
-const App = () => <Compressor />;
+const App = () => {
+  return (
+    <div className='App'>
+      <Compressor />
+    </div>
+  );
+};
 
 export default App;
